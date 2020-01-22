79c2e23ea692f8ddaf719ec99ffb5a87ae3bdc8a
Harper <harperliulove@gmail.com>
2020-01-22 20:54:34 +1100

    bug fixed

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('hello world');
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// const db = require('./models');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public/assets'));
+
+
+// require("./routes/api-routes.js")(app);
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
