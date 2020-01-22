899215f9a02fe4d5b386201d73a5924ce9cd2209
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-01-22 21:46:21 +1100

    convert index.html to handlebars

--- index.js
+++ index.js
@@ -5,18 +5,23 @@
 // *** Dependencies
 // =============================================================
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// const db = require('./models');
-
+// Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static('public/assets'));
+// Set Handlebars.
 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
-// require("./routes/api-routes.js")(app);
+// add static folder.
+app.use('assets', express.static('public/assets'));
+
+app.get('/', (_, res) => res.render('index'));
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
