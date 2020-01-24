c7736ca2ca2d426809ffea5dbd6cf3090cf9ef10
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-01-25 10:39:27 +1100

    added basic routes

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ const exphbs = require("express-handlebars");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+const userRoutes = require("./routes/user_routes");
+const favouriteRoute = require("./routes/favourite_routes");
+const recipeRoute = require("./routes/recipe_routes");
+
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -22,7 +26,17 @@ app.set("view engine", "handlebars");
 // add static folder.
 app.use("assets", express.static("public/assets"));
 
+// Home page
 app.get("/", (_, res) => res.render("index"));
+
+// apis
+app.get("/api/users", userRoutes);
+app.get('api/favourites', favouriteRoute);
+app.get("/api/recipe", recipeRoute);
 
 // eslint-disable-next-line no-console
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+
+app.get("...", function () {
+  console.log("hello");
+});
---
+++ routes/user_routes.js
@@ -0,0 +1,27 @@
+// for /api/users routes
+
+const router = require("express").Router();
+
+// please uncomment this
+// const users = require("../models").user;
+
+// --- GET Routes ---
+// router.get("<<Route>>",(req,res)=>{
+
+// });
+
+// --- POST ---
+// router.post("<<Route>>", (req, res) => {
+
+// });
+
+
+// --- PUT ---
+// router.put("<<ROUTE>>", (req, res) => {
+// });
+
+// --- DELETE ---
+// router.delete("/:id", (req, res) => {
+// });
+
+module.exports = router;
