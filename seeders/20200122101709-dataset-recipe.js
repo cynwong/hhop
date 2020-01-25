03c0ff54d00ed8ec51512fee5d0d8e4edbcbb0a5
tdm <55875354+taraDM23@users.noreply.github.com>
2020-01-25 11:40:01 +1100

    added recipe date set

---
+++ seeders/20200122101709-dataset-recipe.js
@@ -0,0 +1,123 @@
+
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.bulkInsert('recipe', [
+    {
+      title: 'Mango Salsa Salmon',
+      ingredients:
+          '4x 6 ounce salmon steaks \n'
+          + '3 tablespoons olive oil \n'
+          + '1 large red onion, diced \n'
+          + '2 cloves garlic, peeled and minced \n '
+          + '2 tomatoes diced \n 1 sprig fresh cilantro chopped \n'
+          + '2 mangos - peeled, seeded, and diced ',
+      method:
+          'Preheat the oven broiler. Line a broiler pan with foil.\n'
+          + 'Broil salmon steaks on the prepared pan 12 minutes, or until easily flaked with a fork.\n'
+          + 'Heat olive oil in a medium saucepan over medium heat, and saute onion until tender. \n '
+          + 'Stir in garlic, tomatoes, and cilantro. Cook until heated through, 1 to 2 minutes, and remove from heat.'
+          + 'Mix in the mangos. Serve over the salmon steaks.\n',
+      is_private: null,
+      creditTo: null,
+      source: 'https://eightportions.com/datasets/Recipes/#fn:1',
+      photo: 'iA95RTZYD5k0aCdFbZWWGLvipfDukVq',
+    },
+    {
+      title: 'Green Chile and Rice Casserole',
+      ingredients: '1 (6 ounce) package dry instant long grain and wild rice mix \n 1 (8 ounce) container sour cream \n 1 (4 ounce) can chopped green chiles, drained \n 1 (16 ounce) package shredded Cheddar cheese',
+      method: 'Preheat the oven broiler. Line a broiler pan with foil.\nBroil salmon steaks on the prepared pan 12 minutes, or until easily flaked with a fork.\nHeat olive oil in a medium saucepan over medium heat, and saute onion until tender. Stir in garlic, tomatoes, and cilantro. Cook until heated through, 1 to 2 minutes, and remove from heat. Mix in the mangos. Serve over the salmon steaks.\n',
+      is_private: null,
+      creditTo: null,
+      source: 'https://eightportions.com/datasets/Recipes/#fn:1',
+      photo: 'ddluYikmKd1mbiI73JN97uPMOF9En7O',
+    },
+    {
+      title: 'Tex-Mex Pork',
+      ingredients: '1 or 8 ounce can tomato sauce \n 1 cup barbecue sauce \n 1 onion, chopped \n 2 or 4 ounce cans diced green chile peppers \n 1/4 cup chili powder\n 1 teaspoon ground cumin \n 1 teaspoon dried oregano \n 1/4 teaspoon ground cinnamon \n 2 1/2 pounds boneless pork loin roast, trimmed \n 1/2 cup chopped fresh cilantro',
+      method: 'In a 3 quart or larger slow cooker, mix tomato sauce, barbeque sauce, onion, green chile peppers, chili powder, cumin, oregano, and cinnamon. Place pork in slow cooker, and spoon sauce over to coat. the meat.\nCover, and cook on Low 8 to 10 hours, or until pork is tender.\nRemove pork to a cutting board. Using 2 forks, pull meat into shreds. Pour sauce into a serving dish; stir in cilantro and shredded pork.\n',
+      is_private: null,
+      creditTo: null,
+      source: 'https://eightportions.com/datasets/Recipes/#fn:1',
+      photo: 'qkU8KuR63UZvuxktAtA0ezIM25BvMnu',
+    },
+    {
+      title: 'Chinese Fried Rice Deluxe',
+      ingredients: '6 tablespoons lard or cooking oil \n'
+          + '2 eggs  \n'
+          + '1 teaspoon salt  \n'
+          + '2 scallions, chopped  \n'
+          + '1/2 cup fresh shrimp, shelled, deveined and diced  \n'
+          + '4 cups cold, cooked rice  \n'
+          + '1/2 cup cooked peas  \n'
+          + '1/4 cup canned diced bamboo shoots  \n'
+          + '1/4 cup diced boiled ham  \n'
+          + '1/2 cup diced cooked chicken  \n'
+          + '1/2 cup diced Chinese roast pork or left-over roast beef or veal  \n'
+          + '2 tablespoons soy sauce  \n',
+      method: 'Heat 2 tablespoons lard or cooking oil. Beat eggs with 1/2 teaspoon salt and scramble in oil until firm, breaking into small pieces.'
+          + 'Remove and reserve. Heat remaining oil. Add scallion and shrimp and remaining salt.'
+          + 'Cook until shrimp shows pink. Break up lumps of cold cooked rice. Add to shrimp. Stir until rice is heated and the grains of rice are separated.'
+          + 'Make a hole in the center of the rice. Add all the rest of the ingredients except soy sauce.'
+          + 'Stir until thoroughly heated and mixed. Sprinkle soy sauce over rice and mix evenly through. Salt and pepper to taste.'
+          + 'Garnish with additional chopped scallions if desired.\n',
+      is_private: null,
+      creditTo: null,
+      source: 'https://eightportions.com/datasets/Recipes/#fn:1',
+      photo: null,
+    },
+    {
+      title: 'Christmas Eggnog',
+      ingredients: '12 egg whites \n 12 egg yolks \n 1 1 / 2 cups sugar \n 3/4 cup rye whiskey \n 12 egg whites \n  3/4 cup brandy  \n  1/2 cup rum \n  1 to 2 cups heavy cream, lightly whipped \n Garnish: ground nutmeg \n',
+      method: 'Beat the egg whites until stiff, gradually adding in 3/4 cup sugar. Set aside. Beat the egg yolks until they are thick and pale and add the other 3/4 cup sugar and stir in rye whiskey. Blend well. Fold the egg white mixture into the yolk mixture and add the brandy and the rum. Beat the mixture well. To serve, fold the lightly whipped heavy cream into the eggnog. (If a thinner mixture is desired, add the heavy cream unwhipped.) Sprinkle the top of the eggnog with the nutmeg to taste.\n',
+      is_private: null,
+      creditTo: null,
+      source: 'https://eightportions.com/datasets/Recipes/#fn:1',
+      photo: 'ddluYikmKd1mbiI73JN97uPMOF9En7O',
+    },
+    {
+      title: 'Maple Salmon',
+      ingredients: '1/4 cup maple syrup /n 2 tablespoons soy sauce /n 1 clove garlic minced /n 1/4 teaspoon garlic salt /n 1/8 teaspoon ground black pepper /n 1 pound salmon ',
+      method: 'In a small bowl, mix the maple syrup, soy sauce, garlic, garlic salt, and pepper.\nPlace salmon in a shallow glass baking dish, and coat with the maple syrup mixture. Cover the dish, and marinate salmon in the refrigerator 30 minutes, turning once.\nPreheat oven to 400 degrees F (200 degrees C).\nPlace the baking dish in the preheated oven, and bake salmon uncovered 20 minutes, or until easily flaked with a fork.\n',
+      is_private: null,
+      creditTo: null,
+      source: 'https://eightportions.com/datasets/Recipes/#fn:1',
+      photo: ['mKifIaQdD2Ol5.srDFzKh9BLCTxBdXy', '4JGjgjL9NIgOe4Truv5Ku.fKQS7v6q.'],
+    },
+    {
+      title: 'Slow Cooker Beef Stew ',
+      ingredients: '2 pounds beef stew meat, cut into 1 inch cubes /n 1/4 cup all-purpose flour /n 1/2 teaspoon salt /n 1/2 teaspoon ground black pepper /n 1 clove garlic, minced /n 1 bay leaf /n 1 teaspoon paprika /n 1 teaspoon Worcestershire sauce /n 1 onion, chopped /n 1 1/2 cups beef broth /n 3 potatoes, diced  /n 4 carrots, sliced /n 1 stalk celery, chopped',
+      method: 'Place meat in slow cooker. In a small bowl mix together the flour, salt, and pepper; pour over meat, and stir to coat meat with flour mixture. Stir in the garlic, bay leaf, paprika, Worcestershire sauce, onion, beef broth, potatoes, carrots, and celery.\nCover, and cook on Low setting for 10 to 12 hours, or on High setting for 4 to 6 hours.\n',
+      is_private: null,
+      creditTo: null,
+      source: 'https://eightportions.com/datasets/Recipes/#fn:1',
+      photo: null,
+    },
+    {
+      title: "Brooke's Best Bombshell Brownies",
+      ingredients: '1 cup butter, melted \n 3 cups white sugar \n 1 tablespoon vanilla extract \n 4 eggs \n 1 1/2 cups all-purpose flour \n 1 cup unsweetened cocoa powder \n 1 teaspoon salt \n 1 cup semisweet chocolate chips',
+      method: 'Preheat oven to 350 degrees F (175 degrees C). Lightly grease a 9x13 baking dish.\nCombine the melted butter, sugar, and vanilla in a large bowl. Beat in the eggs, one at a time, mixing well after each, until thoroughly blended.\nSift the flour, cocoa powder, and salt in a bowl. Gradually stir flour mixture into the egg mixture until blended. Stir in the chocolate morsels. Spread the batter evenly into the prepared baking dish.\nBake in preheated oven until an inserted toothpick comes out clean, 35 to 40 minutes. Remove, and cool pan on wire rack before cutting.\n',
+      is_private: null,
+      creditTo: null,
+      source: 'https://eightportions.com/datasets/Recipes/#fn:1',
+      photo: 'OYtDo1LyYjZjCw97cooBCXR5af83HCW',
+    },
+    {
+      title: 'Peanut Butter Cup Cookies',
+      ingredients: '1 3/4 cups all-purpose flour \n 1/2 teaspoon salt /n 1 teaspoon baking soda /n 1/2 cup butter, softened \n 1/2 cup white sugar \n 1/2 cup peanut butter \n 1/2 cup packed brown sugar \n 1 egg, beaten \n 1 teaspoon vanilla extract \n 2 tablespoons milk \n 40 miniature chocolate covered peanut butter cups, unwrapped ',
+      method: 'Preheat oven to 375 degrees F (190 degrees C). Sift together the flour, salt and baking soda; set aside.\nCream together the butter, sugar, peanut butter and brown sugar until fluffy. Beat in the egg, vanilla and milk. Add the flour mixture; mix well. Shape into 40 balls and place each into an ungreased mini muffin pan.\nBake at 375 degrees for about 8 minutes. Remove from oven and immediately press a mini peanut butter cup into each ball. Cool and carefully remove from pan.\n',
+      is_private: null,
+      creditTo: null,
+      source: 'https://eightportions.com/datasets/Recipes/#fn:1',
+      photo: 'P/YuaYp5fZLs.mRjujLPHXJ3KiNAwHa',
+    },
+    {
+      title: 'Fluffy French Toast',
+      ingredients: '1/4 cup all-purpose flour \n 1 cup milk \n 1 pinch salt \n 3 eggs \n 1/2 teaspoon ground cinnamon\n 1 teaspoon vanilla extract \n 1 tablespoon white sugar \n 12 thick slices bread',
+      method: 'Measure flour into a large mixing bowl. Slowly whisk in the milk. Whisk in the salt, eggs, cinnamon, vanilla extract and sugar until smooth.\nHeat a lightly oiled griddle or frying pan over medium heat.\nSoak bread slices in mixture until saturated. Cook bread on each side until golden brown. Serve hot.\n',
+      is_private: null,
+      creditTo: null,
+      source: 'https://eightportions.com/datasets/Recipes/#fn:1',
+      photo: 'IHpXGCbuPKEuJMbJ8A0JkeUe67zwHi2',
+    },
+  ]),
+  down: (queryInterface, Sequelize) => queryInterface.bulkDelete('recipe', null, {}),
+};
