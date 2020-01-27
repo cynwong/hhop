409b846cd8e25e37f99c13baa71d02c94043e362
Devin <devin@DevinMac.local>
2020-01-27 19:29:36 +1100

    add user_routes

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -3,12 +3,47 @@
 const router = require("express").Router();
 
 // please uncomment this
-// const users = require("../models").user;
+const users = require("../models").user;
 
 // --- GET Routes ---
-// router.get("<<Route>>",(req,res)=>{
+router.get("/api/login", (req, res) => {
+  users.findOne({
+    where: {
+      email: req.body.email,
+      password: req.body.password,
+    },
+  }).then((dbusers) => {
+    if (dbusers.length === 0) {
+      // send status 400
+      res.json(false);
+    } else {
+      res.json(true);
+    }
+  });
+});
 
-// });
+
+router.post("/api/signup", (req, res) => {
+  users.findOne({
+    where: {
+      email: req.body.email,
+    },
+  }).then((dbusers) => {
+    if (dbusers.length === 0) {
+      res.json(true);
+    } else {
+      // send status 400
+      res.json(false);
+    }
+  });
+  users.create({
+    username: req.body.nameInput,
+    email: req.body.email,
+    password: req.body.password,
+  }).then((dbusers) => {
+    res.json(dbusers);
+  });
+});
 
 // --- POST ---
 // router.post("<<Route>>", (req, res) => {
