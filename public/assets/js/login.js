da4e0b45375d844b84e97e493f5feb3c21dd4f97
Devin <devin@DevinMac.local>
2020-01-27 13:41:37 +1100

    fix bug

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,36 +1,31 @@
-$(document).ready(function () {
-    const email = $("#email").val().trim();
-    const password = $("#password").val().trim();
+$(document).ready(() => {
+  const email = $("#email").val().trim();
+  const password = $("#password").val().trim();
+  const alertext = $("#alertext");
 
-    $(document).on("submit", "signup-form", tryLogin);
-
-    function tryLogin(event) {
-        event.preventDefault();
-        checkPwEm();
+  function validation(res) {
+    if (res) {
+      window.location.href = "/user";
+    } else {
+      alertext.text("Please enter the correct email and password!");
+      email.val("");
+      password.val("");
     }
-
-    function checkPwEm() {
-        if (emailInput != "" && password != "") {
-            $.ajax({
-                url: "/api/login",
-                type: "GET",
-                data: { email, password }
-            }).then((res) => {
-                validation(res);
-            })
-        }
+  }
+  function checkPwEm() {
+    if (email !== "" && password !== "") {
+      $.ajax({
+        url: "/api/login",
+        type: "GET",
+        data: { email, password },
+      }).then((res) => {
+        validation(res);
+      });
     }
-
-    function validation(res) {
-        if (res) {
-            window.location.href = "/user";
-        } else {
-            alertext.text("Please enter the correct email and password!");
-            email.val("");
-            password.val("");
-        }
-        
-    }
-
-    function 
-})
+  }
+  function tryLogin(event) {
+    event.preventDefault();
+    checkPwEm();
+  }
+  $(document).on("submit", "signup-form", tryLogin);
+});
