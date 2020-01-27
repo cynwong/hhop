0dc7759f00681069454ddc7e44883d5ef21274e9
Devin <devin@DevinMac.local>
2020-01-27 20:28:48 +1100

    update

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,3 +1,14 @@
 $(document).ready(() => {
+  const search = $("#search").val.trim();
 
+  function seachRecipe() {
+    $.ajax({
+      url: "/api/search",
+      type: "GET",
+      data: { search },
+    }).then(() => {
+      window.location.href = "/search";
+    });
+  }
+  $(document).on("submit", "#searchForm", seachRecipe);
 });
