4791cf430d9ee8b444afe6df1b59e8475212198f
Devin <devin@DevinMac.local>
2020-01-27 13:42:03 +1100

    fix newuser

--- public/assets/js/newuser.js
+++ public/assets/js/newuser.js
@@ -1,46 +1,44 @@
-$(document).ready(function () {
-    const nameInput = $("#name").val().trim();
-    const email = $("#email").val().trim();
-    const password = $("#password").val().trim();
-    const re_password = $("#re_password").val().trim();
+$(document).ready(() => {
+  const nameInput = $("#name").val().trim();
+  const email = $("#email").val().trim();
+  const password = $("#password").val().trim();
+  const rePassword = $("#rePassword").val().trim();
+  const alertext = $("#alertext");
 
-    $(document).on("submit", "signup-form", signin);
+  function checkUser() {
+    $.ajax({
+      method: "POST",
+      url: "/api/signup",
+      data: { nameInput, email, password },
+    }).then((res) => {
+      if (res) {
+        alertext.text("Sign up successfully! Please log in.");
+      } else {
+        alertext.text("You have already created an account. Please log in.");
+      }
+    });
+  }
 
-    function signin(event) {
-        event.preventDefault();
-        checkInput();
+  function checkPw() {
+    if (password !== rePassword) {
+      alertext.text("Please make enter the same password");
+    } else {
+      checkUser();
     }
+  }
 
-    function checkInput() {
-        if (nameInput != "" && email != "" && password != "" && re_password != "") {
-            checkPw();
-        }else{
-            alertext.text("Please complete the form.");
-        }
+  function checkInput() {
+    if (nameInput !== "" && email !== "" && password !== "" && rePassword !== "") {
+      checkPw();
+    } else {
+      alertext.text("Please complete the form.");
     }
+  }
 
-    function checkPw() {
-        if (password != re_password) {
-            alertext.text("Please make enter the same password");
-        }else{
-            checkUser();
-        }
-    }
+  function signin(event) {
+    event.preventDefault();
+    checkInput();
+  }
 
-    function checkUser() {
-        $.ajax({
-            method: "POST",
-            url: "/api/signup",
-            data: {nameInput, email, password}
-        }).then((res) => {
-            if(res) {
-                alertext.text("Sign up successfully! Please log in.")
-            }else{
-                alertext.text("You have already created an account. Please log in.")
-            }
-        })
-    }
-
-    function
-
-})
+  $(document).on("submit", "signup-form", signin);
+});
