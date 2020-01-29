7130fe642929b4b94fc57f913c268948e52a6db3
Devin <devin@dyn-49-127-133-105.its.monash.edu.au>
2020-01-29 21:26:18 +1100

    fix bug

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,18 +1,18 @@
 $(document).ready(() => {
-  
   function searchRecipe(event) {
     event.preventDefault();
     console.log("1");
     const search = $("#searchText").val().trim();
     console.log(search);
-   
     $.ajax({
       url: "/api/search/:title",
       type: "GET",
-      data: search ,
+      data: search,
     }).then((res) => {
      console.log("search success");
+     console.log(res);
     });
   }
+
   $("#searchForm").on("submit", searchRecipe);
 });
