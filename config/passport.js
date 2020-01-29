f8dfd8e6e2406e1a9db6c359e54b4a1d032d4e61
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-01-30 00:47:23 +1100

    change passport.js to async await

--- config/passport.js
+++ config/passport.js
@@ -1,29 +1,43 @@
+const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcrypt");
 
 // Load User model
 const User = require("../models").user;
 
-module.exports = (passport) => {
-  passport.use(
-    new LocalStrategy({ usernameField: "email" },
-      async (email, password, done) => {
-        const user = await User.findOne({ email });
+passport.use(
+  new LocalStrategy(
+    { usernameField: "email" },
+    async (email, password, done) => {
+      try {
+        // Check if user is registered.
+        const user = await User.findOne({ where: { email } });
         if (!user) {
-          return done(null, false, { message: "Email is not registered." });
+          return done(null, false, { message: "Incorrect Email." });
         }
-        bcrypt.compare(password, user.password, (err, isMatch) => {
-          if (err) throw err;
-          if (isMatch) {
-            return done(null, user);
-          }
-          return done(null, false, { message: "Incorrect Password" });
-        });
-        return null;
-      }),
-  );
+        // Check if password is correct
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (isMatch) {
+          return done(null, user);
+        }
+        return done(null, false, { message: "Incorrect Password" });
+      } catch (err) {
+        // console.error(err);
+        return done(err);
+      }
+    },
+  ),
+);
 
-  passport.serializeUser((user, done) => done(null, user.id));
+passport.serializeUser((user, done) => done(null, user.id));
 
-  passport.deserializeUser((id, done) => User.findByPK(id, (err, user) => done(err, user)));
-};
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findByPk(id);
+    return done(null, user);
+  } catch (err) {
+    return done(err);
+  }
+});
+
+module.exports = passport;
