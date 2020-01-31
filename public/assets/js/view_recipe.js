5999eb6ede1a11a2aa174921dd145b42a91ab5b0
tdm <55875354+taraDM23@users.noreply.github.com>
2020-01-31 16:14:45 +1100

    uncommented delete function code

--- public/assets/js/view_recipe.js
+++ public/assets/js/view_recipe.js
@@ -4,23 +4,23 @@ $(document).ready(() => {
   //
   //
   // Delete recipe -- Only user submitted
-  /* function DELETE() {
-      const listItemData = $(this).parent("#").parent("#").data("recipe");
-      const id = listItemData.id;
-      $.ajax({
-        method: "DELETE",
-        url: "/api/delete/" + id
-      })
+  function DELETE() {
+    const listItemData = $(this).parent("#").parent("#").data("recipe");
+    const id = listItemData.id;
+    $.ajax({
+      method: "DELETE",
+      url: "/api/delete/" + id
+    })
       .then((res) => {
-          if (res) {
-            // check if user posted recipe
-            //
-            //
-          } else {
-            alertText.text("You do not have authority to delete this recipe");
-          }
-        });
-    }
-   */
-  // $(document).on("delete", "#delete", DELETE);
+        if (res) {
+          // check if user posted recipe
+          //
+          //
+        } else {
+          alertText.text("You do not have authority to delete this recipe");
+        }
+      });
+  }
+
+  $(document).on("delete", "#delete", DELETE);
 });
