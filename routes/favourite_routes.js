b15e364b934740bafa7fbbc94a6d800e1fed399b
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-01-31 23:29:45 +1100

    modify fav code to include favCount

--- routes/favourite_routes.js
+++ routes/favourite_routes.js
@@ -1,32 +1,59 @@
 // for /api/favourite routes
 const router = require("express").Router();
 
+const { checkAuthenticated } = require("../config/auth");
+const { ViewMyFavourites } = require("../config/page_settings");
+
+// get the models
 const models = require("../models");
 
 const Favourites = models.favourite;
 const Recipe = models.recipe;
 
 // --- GET Routes ---
-router.get("/", async (req, res) => {
+router.get("/", checkAuthenticated, async (req, res) => {
+  // get current user id
+  const { user } = req;
+  const userId = user.id;
+
+  // get user's favourite records
   const favs = await Favourites.findAll({
-    attributes: [],
     where: {
-      userId: 1,
+      userId,
     },
     include: {
       model: Recipe,
       require: true,
+      include: {
+        model: Favourites,
+      },
     },
   });
-  const recipes = favs.map(({ dataValues }) => ({
-    recipe: dataValues.recipe.dataValues,
-  }));
-  res.render("view_favourites", {
-    title: "Recipe Lover!: View favourites",
-    recipes,
-    isMain: true,
-    isSearch: true,
+
+  // get recipes data for rendition
+  const recipes = favs.map(({ dataValues }) => {
+    const {
+      id,
+      title,
+      photo,
+      favourites,
+    } = dataValues.recipe;
+    const recipe = {
+      id,
+      title,
+      photo,
+    };
+    recipe.favCount = favourites.length;
+    return recipe;
   });
+
+  // construct view page.
+  const pageSettings = {
+    ...ViewMyFavourites,
+    user,
+    recipes,
+  };
+  res.render("view_favourites", pageSettings);
 });
 
 // --- POST ---
