4b76164eb29d20b8925744c48d4eb836372a6d6b
Devin <devin@DevinMac.local>
2020-01-31 18:01:43 +1100

    create search route

--- routes/recipe_routes.js
+++ routes/recipe_routes.js
@@ -1,8 +1,10 @@
 // for /recipe routes
 
 const router = require("express").Router();
+const Sequelize = require('sequelize');
+const Recipes = require("../models").recipe;
 
-// const Recipes = require("../models").recipe;
+const { Op } = Sequelize;
 
 // --- GET Routes ---
 // route "/recipe" : Recipe page
@@ -13,11 +15,35 @@ router.get("/", (_, res) => res.render("view_recipe", {
 }));
 
 // route "/recipe/search" : Search page
-router.get("/search", (_, res) => res.render("search_recipe", {
-  title: "Recipe Lovers!: view Search",
-  isMain: true,
-  isSearch: true,
-}));
+// router.get("/search", (_, res) => res.render("search_recipe", {
+//   title: "Recipe Lovers!: view Search",
+//   isMain: true,
+//   isSearch: true,
+// }));
+
+
+// post request for recipe search
+router.get("/search/:title", async (req, res) => {
+  const result = await Recipes.findAll({
+    where: {
+      title: {
+        [Op.like]: "%".concat(req.params.title, "%"),
+      },
+    },
+  });
+  console.log(result);
+  const recipes = result.map(({ dataValues }) => ({
+    id: dataValues.id,
+    authorId: dataValues.authorId,
+    recipe: dataValues,
+  }));
+  console.log(result[0].dataValues);
+  res.render("search_recipe", {
+    title: "View Search",
+    result: recipes,
+    isSearch: true,
+  });
+});
 
 // --- POST ---
 // router.post("<<Route>>", (req, res) => {
