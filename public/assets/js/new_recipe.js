42a4d038f69f9e921d406cecde293323df9d75e3
tdm <55875354+taraDM23@users.noreply.github.com>
2020-01-31 13:42:47 +1100

    link hbs file to js file

--- public/assets/js/new_recipe.js
+++ public/assets/js/new_recipe.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
 
-var titleInput = ;
+var title = $("#newTitle").val().trim(); 
+var Ingredients = $("#newIngredients").val().trim();
+var Method = $("#newMethod").val().trim();
+var Credit = $("#newCredit").val().trim();
+var Source = $("#newSource").val().trim();
+var addImage = $("#newImage").val().trim();
 
-
-
-
-
-
+$(document).on("submit", "#newRecipe", addRecipe);
 
 }
