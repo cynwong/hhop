8c4a1621e22b7e78e33d53b89decdd4f2dd72fb0
Devin <devin@dyn-49-127-158-224.its.monash.edu.au>
2020-02-01 10:41:04 +1100

    delete consol.log

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,18 +1,17 @@
 $(document).ready(() => {
   function searchRecipe(event) {
     event.preventDefault();
-    console.log("1");
     const search = $("#searchText").val().trim();
     const path = "/recipe/search/";
-    // console.log(search);
+    const notice = $("#noResult");
     $.ajax({
       url: path.concat(search),
       type: "GET",
     }).then((res) => {
-      console.log("search success");
-      window.location.href = path.concat(search);;
-      console.log(window.location.href);
-      console.log("the results on client side is: ", res);
+      window.location.href = path.concat(search);
+      if(res.length === 0) {
+        notice.text("No result");
+      }
     });
   }
 
