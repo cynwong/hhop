0c2e91a05ba6585cbe9897d9d6a86760aae144fc
tdm <55875354+taraDM23@users.noreply.github.com>
2020-02-01 11:10:38 +1100

    removed code to fix js hierachy issue

--- public/assets/js/new_recipe.js
+++ public/assets/js/new_recipe.js
@@ -1,39 +1,46 @@
-$(document).ready(() => {
-  const title = $("#newTitle").val().trim();
-  const ingredients = $("#newIngredients").val().trim();
-  const method = $("#newMethod").val().trim();
-  const credit = $("#newCredit").val().trim();
-  const source = $("#newSource").val().trim();
-  const addImage = $("#newImage").val().trim();
-  const alertText = $("#alert-text");
+// $(document).ready(() => {
 
-  function newRecipe() {
-    $.ajax({
-      method: "POST",
-      url: "/api/add",
-      data: {
-        title, ingredients, method, credit, source, addImage,
-      },
-    }).then((res) => {
-      if (res) {
-        alertText.text("New Recipe added Successfully");
-      }
-    });
-  }
+const title = $("#newTitle");
+const ingredients = $("#newIngredients");
+const method = $("#newMethod");
+const credit = $("#newCredit");
+const source = $("#newSource");
+const addImage = $("#newImage");
+const alertText = $("#alert-text");
+// const submit = $("#submit");
 
-  function runCheck() {
-    if (title !== "" && ingredients !== "" && method !== "") {
-      newRecipe();
-    } else {
-      alertText.text("Please check if all required information has been filled");
+function newRecipe() {
+  $.ajax({
+    method: "POST",
+    url: "/api/add",
+    data: {
+      title, ingredients, method, credit, source, addImage,
+    },
+  }).then((res) => {
+    if (res) {
+      alertText.text("New Recipe added Successfully");
     }
+  });
+}
+// eslint-disable-next-line max-len
+// bring both functions together --- grab the data as array and check against the array and then-- display and error
+function runCheck() {
+  if (title !== "" && ingredients !== "" && method !== "") {
+    newRecipe();
+  } else {
+    alertText.text("Please check if all required information has been filled");
   }
+}
 
-  function submit(event) {
-    event.preventDefault();
-    runCheck();
-  }
+function submitRecipe(event) {
+  event.preventDefault();
+  runCheck();
+}
 
 
-  $(document).on("submit", "#newRecipe", submit);
-});
+// $("#newRecipe").on("click", (submit));
+// $("#newRecipe").on("click", "#submit", submitRecipe);
+
+// document.getElementByID("submit").addEventListener("click", submitRecipe);
+// $(document).on("submit", "#newRecipe", submit);
+// });
