2387f713dbdb4f323b482425f479b7aa15c8d2bc
tdm <55875354+taraDM23@users.noreply.github.com>
2020-02-02 00:39:38 +1100

    added method datto view recipe

--- public/assets/js/new_recipe.js
+++ public/assets/js/new_recipe.js
@@ -1,31 +1,51 @@
-const title = $("#newTitle");
-const ingredients = $("#newIngredients");
-const method = $("#newMethod");
-const credit = $("#newCredit");
-const source = $("#newSource");
-const addImage = $("#newImage");
-const alertText = $("#alert-text");
+/* eslint-disable no-undef */
+$(document).ready(() => {
+  $("#submit").click(async (event) => {
+    event.preventDefault();
+    const title = $("#newTitle");
+    const ingredients = $("#newIngredients");
+    const method = $("#newMethod");
+    const credit = $("#newCredit");
+    const source = $("#newSource");
+    const addImage = $("#newImage");
+    const errors = [];
 
-$("#submit").on("click", (event) => {
-  event.preventDefault();
-  const data = {
-    title: title.val().trim(),
-    ingredients: ingredients.val().trim(),
-    method: method.val().trim(),
-    creditTo: credit.val().trim(),
-    source: source.val().trim(),
-    photo: addImage.val().trim(),
-  };
-  $.ajax({
-    method: "POST",
-    url: "/recipe/add",
-    data,
-  }).then((res) => {
-    if (res) {
-      alertText.text("Your new recipe has been added!");
-      setTimeout(window.location.replace("/recipe/add"), 6000);
-    } if (res.error) {
-      alertText.text("Please check if all required information has been filled");
+    const data = {
+      title: title.val().trim(),
+      ingredients: ingredients.val().trim(),
+      method: method.val().trim(),
+      creditTo: credit.val().trim(),
+      source: source.val().trim(),
+      photo: addImage.val().trim(),
+    };
+
+    const titleErr = title.val().trim();
+    if (!titleErr) errors.push({ msg: "Please enter a title." });
+
+    const ingredientsErr = ingredients.val().trim();
+    if (!ingredientsErr) errors.push({ msg: "Please enter a ingredients." });
+
+    const methodErr = method.val().trim();
+    if (!methodErr) errors.push({ msg: "Please enter method." });
+
+    if (errors.length > 0) {
+      // eslint-disable-next-line no-undef
+      return alertUser(errors);
+    }
+
+    try {
+      const response = await $.ajax({
+        method: "POST",
+        url: "/recipe/add",
+        data,
+      });
+      if (response.error) {
+        return alertUser(response.error);
+      }
+      return window.location.replace("/recipe/add");
+    } catch (error) {
+      // eslint-disable-next-line no-undef
+      return alertUser(error.responseJSON.error);
     }
   });
 });
