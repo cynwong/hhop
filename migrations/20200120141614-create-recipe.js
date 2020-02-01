9ed6677eaa91837e974c03d88ba017b13c0a6bf3
tdm <55875354+taraDM23@users.noreply.github.com>
2020-02-01 13:17:53 +1100

    updated recipe table to accept image url

--- migrations/20200120141614-create-recipe.js
+++ migrations/20200120141614-create-recipe.js
@@ -50,9 +50,11 @@ module.exports = {
       },
     },
     photo: {
-      type: Sequelize.TEXT,
+      type: Sequelize.STRING(255),
       allowNull: true,
-    },
+      validate: {
+        is: /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)/gi,
+      },
     createdAt: {
       allowNull: false,
       type: Sequelize.DATE,
