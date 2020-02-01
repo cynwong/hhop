8db090e7eca96b3d397ff6d7ade6af66e81183e0
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-02-01 22:55:29 +1100

    try with sequelize uri

--- models/index.js
+++ models/index.js
@@ -3,20 +3,27 @@ const path = require("path");
 const Sequelize = require("sequelize");
 
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || "development";
-// eslint-disable-next-line import/no-dynamic-require
-let configFile;
-if (process.env.JAWSDB_URL) {
-  configFile = path.resolve(__dirname, "..", "config", "config.production.json");
-} else {
-  configFile = path.resolve(__dirname, "..", "config", "config.json");
-}
+// // eslint-disable-next-line import/no-dynamic-require
+// let configFile;
+// if (process.env.JAWSDB_URL) {
+//   configFile = path.resolve(__dirname, "..", "config", "config.production.json");
+// } else {
+//   configFile = path.resolve(__dirname, "..", "config", "config.json");
+// }
 
-// eslint-disable-next-line import/no-dynamic-require
-const config = require(configFile)[env];
+// // eslint-disable-next-line import/no-dynamic-require
+// const config = require(configFile)[env];
 const db = {};
 
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
+// const sequelize = new Sequelize(config.database, config.username, config.password, config);
+let sequelize;
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL, {});
+} else {
+  const env = process.env.NODE_ENV || "development";
+  const config = path.resolve(__dirname, "..", "config", "config.json")[env];
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 fs
   .readdirSync(__dirname)
