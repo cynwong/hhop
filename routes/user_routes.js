10613f2ea18aaf53c94ae471c1efe923136a887e
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-02-02 00:18:35 +1100

    re-orgainize the user routes

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -24,6 +24,41 @@ router.get("/", checkAuthenticated, (req, res) => {
   return res.render("user", pageSettings);
 });
 
+// route "/user/login" : User log-in page
+router.route("/login")
+  .get(forwardAuthenticated, (_, res) => res.render("login", LoginPageSettings))
+  .post((req, res, next) => {
+    // check if user has authorization
+    passport.authenticate("local", (err, user) => {
+      if (err) {
+        console.error(err);
+        return next(err);
+      }
+      if (!user) {
+        return res.status(401).json({
+          error: [{ msg: "Incorrect username or password" }],
+        });
+      }
+      // log in User.
+      req.logIn(user, (logInErr) => {
+        if (logInErr) {
+          console.error(logInErr);
+          return next(logInErr);
+        }
+        return res.status(200).json({
+          success_msg: "Login successful.",
+        });
+      });
+      return null;
+    })(req, res, next);
+  });
+
+// route:"/user/logout" : Logout
+router.get("/logout", (req, res) => {
+  req.logOut();
+  res.redirect("/user/login");
+});
+
 // route "/user/password"
 router.route("/password")
   .get(checkAuthenticated, (req, res) => {
@@ -65,98 +100,61 @@ router.route("/password")
     }
   });
 
-// route "/user/login" : User log-in page
-router.get("/login", forwardAuthenticated, (_, res) => res.render("login", LoginPageSettings));
-
-// route:"/user/logout" : Logout
-router.get("/logout", (req, res) => {
-  req.logOut();
-  res.redirect("/user/login");
-});
-
 // route "/user/register" : User Registration page.
-router.get("/register", forwardAuthenticated, (_, res) => res.render("register_user", RegisterUserPageSettings));
+router.route("/register")
+  .get(forwardAuthenticated, (_, res) => res.render("register_user", RegisterUserPageSettings))
+  .post(async (req, res) => {
+    try {
+      const {
+        email,
+        password,
+        name,
+      } = req.body;
+      const errors = [];
 
-// --- http Request - POST ---
-// route "/user/login" : Login
-router.post("/login", (req, res, next) => {
-  // check if user has authorization
-  passport.authenticate("local", (err, user) => {
-    if (err) {
-      console.error(err);
-      return next(err);
-    }
-    if (!user) {
-      return res.status(401).json({
-        error: [{ msg: "Incorrect username or password" }],
-      });
-    }
-    // log in User.
-    req.logIn(user, (logInErr) => {
-      if (logInErr) {
-        console.error(logInErr);
-        return next(logInErr);
+      // data validation
+      if (!email) errors.push({ msg: "Email is required. " });
+      if (!password) errors.push({ msg: "Password is required. " });
+      if (!name) errors.push({ msg: "Name is required. " });
+
+      if (password.length < 8) {
+        errors.push({ msg: "Password must be at least 8 characters long" });
       }
+
+      if (errors.length > 0) {
+        // if there is errors, send it back to browser.
+        return res.status(400).json({
+          error: errors,
+        });
+      }
+
+      // check if user already registered.
+      const user = await Users.findOne({
+        where: { email },
+      });
+      if (user) {
+        // if user, then notify the client.
+        return res.status(400).json({
+          error: [{ msg: "User already exists." }],
+        });
+      }
+
+      // add new user to db
+      await Users.create({
+        email,
+        name,
+        password,
+      });
+      // return success code back to browser
       return res.status(200).json({
-        success_msg: "Login successful.",
+        success_msg: "Successfully registered",
       });
-    });
-    return null;
-  })(req, res, next);
-});
-
-router.post("/register", async (req, res) => {
-  try {
-    const {
-      email,
-      password,
-      name,
-    } = req.body;
-    const errors = [];
-
-    // data validation
-    if (!email) errors.push({ msg: "Email is required. " });
-    if (!password) errors.push({ msg: "Password is required. " });
-    if (!name) errors.push({ msg: "Name is required. " });
-
-    if (password.length < 8) {
-      errors.push({ msg: "Password must be at least 8 characters long" });
-    }
-
-    if (errors.length > 0) {
-      // if there is errors, send it back to browser.
-      return res.status(400).json({
-        error: errors,
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({
+        error: [{ msg: "Something went wrong. Try again later." }],
       });
     }
-
-    // check if user already registered.
-    const user = await Users.findOne({
-      where: { email },
-    });
-    if (user) {
-      // if user, then notify the client.
-      return res.status(400).json({
-        error: [{ msg: "User already exists." }],
-      });
-    }
-
-    // add new user to db
-    await Users.create({
-      email,
-      name,
-      password,
-    });
-    // return success code back to browser
-    return res.status(200).json({
-      success_msg: "Successfully registered",
-    });
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({
-      error: [{ msg: "Something went wrong. Try again later." }],
-    });
-  }
-});
+  });
 
 module.exports = router;
