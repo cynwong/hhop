27acb2a212d1488ae49a54aa92b3e2516d89bac5
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-02-01 23:20:57 +1100

    remove saved unused code

--- models/index.js
+++ models/index.js
@@ -3,21 +3,13 @@ const path = require("path");
 const Sequelize = require("sequelize");
 
 const basename = path.basename(__filename);
-// // eslint-disable-next-line import/no-dynamic-require
-// let configFile;
-// if (process.env.JAWSDB_URL) {
-//   configFile = path.resolve(__dirname, "..", "config", "config.production.json");
-// } else {
-//   configFile = path.resolve(__dirname, "..", "config", "config.json");
-// }
 
-// // eslint-disable-next-line import/no-dynamic-require
-// const config = require(configFile)[env];
+// DB setup
 const db = {};
 
-// const sequelize = new Sequelize(config.database, config.username, config.password, config);
 let sequelize;
 if (process.env.JAWSDB_URL) {
+  // for Heroku
   sequelize = new Sequelize(process.env.JAWSDB_URL, {});
 } else {
   const env = process.env.NODE_ENV || "development";
