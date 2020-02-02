37e3c37253e263d26a850eae14073937f4cfe1d6
tdm <55875354+taraDM23@users.noreply.github.com>
2020-02-02 12:31:28 +1100

    building delete fucntion

--- public/assets/js/view_recipe.js
+++ public/assets/js/view_recipe.js
@@ -1,24 +1,30 @@
-$(document).ready(() => {
-  const alertText = $("#alert-text");
-  // Update existing recipe
-  //
-  //
-  // Delete recipe -- Only user submitted
-  function deleteRecipe() {
-    $.ajax({
-      method: "DELETE",
-      url: "/api/delete/",
-    })
-      .then((res) => {
-        if (res) {
-          // check if user posted recipe
-          //
-          //
-        } else {
-          alertText.text("You do not have authority to delete this recipe");
-        }
-      });
-  }
 
-  $(document).on("delete", "#delete", deleteRecipe);
+// $("#delete").click( deleteTodo ) => {
+$("#delete").click(function (event) {
+  const id = $(this).data("id");
+  // if (this = authorId)
+  // eslint-disable-next-line no-shadow
+  function getRecipes() {
+    // eslint-disable-next-line prefer-arrow-callback
+    $.get("/recipe", function (data) {
+      // eslint-disable-next-line no-undef
+      recipe = data;
+    });
+  }
+  // function deleteTodo(event) {
+  event.stopPropagation();
+  // const id = $(this).data("id");
+  $.ajax({
+    method: "DELETE",
+    // eslint-disable-next-line prefer-template
+    url: "/recipe/:" + id,
+  }).then(getRecipes);
 });
+// eslint-disable-next-line no-unused-vars
+function getRecipes() {
+  // eslint-disable-next-line prefer-arrow-callback
+  $.get("/recipe", function (data) {
+    // eslint-disable-next-line no-undef
+    recipe = data;
+  });
+}
