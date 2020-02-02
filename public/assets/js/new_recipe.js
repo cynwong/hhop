56457683179f636e449ed98b37ad68678a943ce5
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2020-02-03 10:04:15 +1100

    remove isPrivate from add new form

--- public/assets/js/new_recipe.js
+++ public/assets/js/new_recipe.js
@@ -1,34 +1,20 @@
-/* eslint-disable no-undef */
 $(document).ready(() => {
   $("#submit").click(async (event) => {
     event.preventDefault();
-    const title = $("#newTitle");
-    const ingredients = $("#newIngredients");
-    const method = $("#newMethod");
-    const credit = $("#newCredit");
-    const source = $("#newSource");
-    const addImage = $("#newImage");
-    const isPvt = $("isPvt");
+    const title = $("#newTitle").val().trim();
+    const ingredients = $("#newIngredients").val().trim();
+    const method = $("#newMethod").val().trim();
+    const creditTo = $("#newCredit").val().trim();
+    const source = $("#newSource").val().trim();
+    const photo = $("#newImage").val().trim();
     const errors = [];
 
-    const data = {
-      title: title.val().trim(),
-      ingredients: ingredients.val().trim(),
-      method: method.val().trim(),
-      creditTo: credit.val().trim(),
-      source: source.val().trim(),
-      photo: addImage.val().trim(),
-      isPvt: isPvt.val(),
-    };
+    // validation
+    if (!title) errors.push({ msg: "Please enter a title." });
 
-    const titleErr = title.val().trim();
-    if (!titleErr) errors.push({ msg: "Please enter a title." });
+    if (!ingredients) errors.push({ msg: "Please enter a ingredients." });
 
-    const ingredientsErr = ingredients.val().trim();
-    if (!ingredientsErr) errors.push({ msg: "Please enter a ingredients." });
-
-    const methodErr = method.val().trim();
-    if (!methodErr) errors.push({ msg: "Please enter method." });
+    if (!method) errors.push({ msg: "Please enter method." });
 
     if (errors.length > 0) {
       // eslint-disable-next-line no-undef
@@ -36,12 +22,21 @@ $(document).ready(() => {
     }
 
     try {
+      // save the data
       const response = await $.ajax({
         method: "POST",
         url: "/recipe/add",
-        data,
+        data: {
+          title,
+          ingredients,
+          method,
+          creditTo,
+          source,
+          photo,
+        },
       });
       if (response.error) {
+        // eslint-disable-next-line no-undef
         return alertUser(response.error);
       }
       return window.location.replace(`/recipe/${response.data.recipeId}`);
